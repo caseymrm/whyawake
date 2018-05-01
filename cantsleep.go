5d659187cae78928cfc191a9d76744c33d4b9c21
Casey Muller <github@nullterminated.com>
2018-05-01 15:42:11 -0700

    Run on startup

--- cantsleep.go
+++ cantsleep.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"os/user"
+	"path/filepath"
 	"strconv"
 	"syscall"
 
@@ -26,8 +30,59 @@ func canSleep() bool {
 	return true
 }
 
+func getStartupPath() string {
+	u, err := user.Current()
+	if err != nil {
+		log.Printf("user.Current: %v", err)
+		return ""
+	}
+	return u.HomeDir + "/Library/LaunchAgents/com.github.caseymrm.CantSleep.plist"
+}
+
+func runningAtStartup() bool {
+	_, err := os.Stat(getStartupPath())
+	if err == nil {
+		return true
+	}
+	return false
+}
+
+func removeStartupItem() {
+	err := os.Remove(getStartupPath())
+	if err != nil {
+		log.Printf("os.Remove: %v", err)
+	}
+}
+
+func addStartupItem() {
+	path := getStartupPath()
+	// Make sure ~/Library/LaunchAgents exists
+	err := os.MkdirAll(filepath.Dir(path), 0700)
+	if err != nil {
+		log.Printf("os.MkdirAll: %v", err)
+		return
+	}
+	executable, err := os.Executable()
+	if err != nil {
+		log.Printf("os.Executable: %v", err)
+		return
+	}
+	fmt.Println(executable)
+	f, err := os.Create(path)
+	if err != nil {
+		log.Printf("os.Create: %v", err)
+		return
+	}
+	defer f.Close()
+	_, err = f.WriteString(fmt.Sprintf(launchdConfig, executable))
+	if err != nil {
+		log.Printf("f.WriteString: %v", err)
+		return
+	}
+}
+
 func menuItems() []tray.MenuItem {
-	items := make([]tray.MenuItem, 0, 1)
+	items := make([]tray.MenuItem, 0)
 	pidAsserts := assertions.GetPIDAssertions()
 	for key := range sleepKeywords {
 		pids := pidAsserts[key]
@@ -47,6 +102,12 @@ func menuItems() []tray.MenuItem {
 	if len(items) > 0 {
 		preAmble = append(preAmble, tray.MenuItem{Text: "---"})
 	}
+	startupItem := tray.MenuItem{Text: "Run at start up", Callback: "startup"}
+	if runningAtStartup() {
+		startupItem.State = true
+	}
+	items = append(items, startupItem)
+
 	return append(preAmble, items...)
 }
 
@@ -70,18 +131,27 @@ func monitorAssertionChanges(channel chan assertions.AssertionChange) {
 }
 
 func handleClicks(callback chan string) {
-	for pidString := range callback {
-		pid, _ := strconv.Atoi(pidString)
-		go func() {
-			switch tray.App().Alert("Kill process?", fmt.Sprintf("PID %d", pid), "Kill", "Kill -9", "Cancel") {
-			case 0:
-				fmt.Printf("Killing pid %d\n", pid)
-				syscall.Kill(pid, syscall.SIGTERM)
-			case 1:
-				fmt.Printf("Killing -9 pid %d\n", pid)
-				syscall.Kill(pid, syscall.SIGKILL)
+	for clicked := range callback {
+		switch clicked {
+		case "startup":
+			if runningAtStartup() {
+				removeStartupItem()
+			} else {
+				addStartupItem()
 			}
-		}()
+		default:
+			pid, _ := strconv.Atoi(clicked)
+			go func() {
+				switch tray.App().Alert("Kill process?", fmt.Sprintf("PID %d", pid), "Kill", "Kill -9", "Cancel") {
+				case 0:
+					fmt.Printf("Killing pid %d\n", pid)
+					syscall.Kill(pid, syscall.SIGTERM)
+				case 1:
+					fmt.Printf("Killing -9 pid %d\n", pid)
+					syscall.Kill(pid, syscall.SIGKILL)
+				}
+			}()
+		}
 	}
 }
 
@@ -99,3 +169,18 @@ func main() {
 	go handleClicks(trayChannel)
 	app.RunApplication()
 }
+
+var launchdConfig = `
+<?xml version='1.0' encoding='UTF-8'?>
+ <!DOCTYPE plist PUBLIC \"-//Apple Computer//DTD PLIST 1.0//EN\" \"http://www.apple.com/DTDs/PropertyList-1.0.dtd\" >
+ <plist version='1.0'>
+   <dict>
+     <key>Label</key><string>CantSleep</string>
+     <key>Program</key><string>%s</string>
+     <key>StandardOutPath</key><string>/tmp/CantSleep-out.log</string>
+     <key>StandardErrorPath</key><string>/tmp/CantSleep-err.log</string>
+     <key>KeepAlive</key><true/>
+     <key>RunAtLoad</key><true/>
+   </dict>
+</plist>
+`
